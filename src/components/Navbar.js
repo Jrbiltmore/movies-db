6cd3c08114106c7599cc4c4a0ae2991bb3e55f2a
majidalykhan <majidalykhan@gmail.com>
2021-06-16 15:44:56 +0500

    About page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img src={logo} alt="Movies DB Logo" className="logo"></img>
+        </Link>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">About</h1>
+      <p>React Movies Database App</p>
+    </section>
   );
 };
 
