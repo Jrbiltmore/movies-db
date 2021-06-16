2044e028bec2cd12273fbac86549692adf7f24bf
majidalykhan <majidalykhan@gmail.com>
2021-06-16 15:17:21 +0500

    Routes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SingleCocktail from "./pages/SingleMovie";
 import Error from "./pages/Error";
+
 // import components
 import Navbar from "./components/Navbar";
+import SingleMovie from "./pages/SingleMovie";
+
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/movie/:id">
+          <SingleMovie />
+        </Route>
+        <Route exact path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
